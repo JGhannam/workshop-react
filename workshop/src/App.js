8fd489b914c69fbcae3841d6919a01e44d498089
GriffinZaOriginal <97244554+GriffinZaOriginal@users.noreply.github.com>
2022-06-21 20:17:56 +0300

    idk

--- workshop/src/App.js
+++ workshop/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import './App.css'
 
 export default class App extends Component {
     state = {
@@ -8,7 +9,7 @@ export default class App extends Component {
     render() {
         return (
             <div>      
-            <h1>{this.state.counter}</h1>
+            <h1 className={this.state.counter }>{this.state.counter}</h1>
             <button onClick={() => this.setState({ counter: this.state.counter + 1 })}> 
                 +
             </button> 
