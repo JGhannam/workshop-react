73cf5e2bdbf3ac7dbd12a3adac48eb8a6b2d717a
alifouani1 <fouaniali08@gmail.com>
2022-06-22 08:51:11 +0300

    some editings

--- workshop/src/App.js
+++ workshop/src/App.js
@@ -1,34 +1,53 @@
-import React, { Component } from 'react';
-import './App.css'
+import React, { Component } from "react";
+import "./App.css";
 
 export default class App extends Component {
-    state = {
-        counter: 0,
-    }
-    
-    render() {
-        return (
-            <div>      
-            <h1 className={this.state.counter }>{this.state.counter}</h1>
-            <button onClick={() => this.setState({ counter: this.state.counter + 1 })}> 
-                +
-            </button> 
-            <button onClick={() => this.setState({ counter: this.state.counter - 1 })}> 
-                -
-            </button>
-            </div>
-        );
-    }
+  state = {
+    counter: 0,
+  };
 
-    addtoCounter = () => {
-        this.setState({
-            counter: this.state.counter + 1,
-        });
-    }
+  render() {
+    return (
+      <div style={{ width: "50%", marginLeft: "35%" }}>
+        <h1>Life Damage</h1>
+        <div style={{ color: "blue", padding: 20 }}>
+          <h1 className={this.state.counter}>{this.state.counter}</h1>
+          <button
+            onClick={() => this.setState({ counter: this.state.counter + 1 })}
+          >
+            +
+          </button>
+          <button
+            onClick={() => this.setState({ counter: this.state.counter - 1 })}
+          >
+            -
+          </button>
+        </div>
+        <div>
+          <button
+            style={{
+              backgroundColor: "aqua",
+              color: "blue",
+              width: "0.5%",
+              padding: 50,
+              borderRadius: "50%",
+            }}
+            onClick={() => this.setState({ counter: this.state.counter - 1 })}
+          ></button>
+        </div>
+      </div>
+    );
+  }
 
-    subtractFromCounter = () => {
-        this.setState({
-            counter: this.state.counter - 1,
-        });
-    }
+  addtoCounter = () => {
+    this.setState({
+      counter: this.state.counter + 1,
+    });
+  };
+
+  subtractFromCounter = () => {
+    this.setState({
+      counter: this.state.counter - 1,
+    });
+  };
 }
