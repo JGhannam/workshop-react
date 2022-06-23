ab7f531cc2986b06bfb1a351c2afc907ab10900b
GriffinZaOriginal <97244554+GriffinZaOriginal@users.noreply.github.com>
2022-06-23 11:49:55 +0300

    done!!

--- workshop/src/App.js
+++ workshop/src/App.js
@@ -1,74 +1,33 @@
 import React, { Component } from "react";
 import "./App.css";
+import Enemy from "./components/enemy";
 
-export default class App extends Component {
+class App extends Component {
   state = {
-    counter: 100,
+    enemycount: [
+      {
+        number: 1,
+      },
+      {
+        number: 2,
+      },
+      {
+        number: 3,
+      },
+      {
+        number: 4,
+      },
+    ]
   };
+
 
   render() {
     return (
-      <React.Fragment>
-
-        <h1>Life Damage</h1> 
-          
-          <div>
-            <span>enemy status: </span>
-            <span>{this.isDead() ? 'Killed :) ' : 'LOL you lame'}</span>
-              
-          </div>
-    
-          <h1 className={this.state.counter < 0 ? 'underZero' : 'aboveZero'}>{this.state.counter}</h1>
-
-          {/* <button
-            onClick={() => this.setState({ counter: this.state.counter + 1 })}
-            className={"buttonAdd"}
-          >
-            +
-          </button> */}
-
-          <button
-            onClick={() => this.setState({ counter: this.state.counter - 1 })}
-            className="buttonMinus"
-          >
-            -
-          </button>
-
-          <button
-            onClick={() => this.setState({ counter: this.state.counter - 10 })}
-            className="buttonMinus"
-          >
-            -10
-          </button>            
-
-          <button className="buttonReset" onClick={() => this.setState({ counter: 100 })}>
-            Reset
-          </button>
-
-        <div className="divDamage" onClick={ () => this.setState({ counter: this.state.counter - 100})}></div>
-      </React.Fragment>
+    this.state.enemycount.map(enemy => (
+      <Enemy key={enemy.number} number={enemy.number} />
+    ))
     );
   }
-
-  addtoCounter = () => {
-    this.setState({
-      counter: this.state.counter + 1,
-    });
-    this.isDead();
-  };
-
-  subtractFromCounter = () => {
-    this.setState({
-      counter: this.state.counter - 1,
-    });
-    this.isDead();
-  };
-
-  isDead = () => {
-    if (this.state.counter < 0) {
-      return true;
-    } else {
-      return false;
-    }
-  }    
 }
+
+export default App;
---
+++ workshop/src/components/enemy.js
@@ -0,0 +1,76 @@
+import React, { Component } from "react";
+import "./styling/enemy.css";
+
+export default class Enemy extends Component {
+  state = {
+    counter: 100,
+  };
+
+  render() {
+    return (
+      <div>
+        
+        <div className={this.isDead() ? 'enemyKilled' : 'enemyNotKilled'}>
+        <h1>Life Damage Enemy {this.props.number}</h1> 
+          
+          <div>
+            <span>enemy status: </span>
+            <span>{this.isDead() ? 'Killed :) ' : 'LOL you lame'}</span>
+              
+          </div>
+    
+          <h1 className={this.state.counter < 0 ? 'underZero' : 'aboveZero'}>{this.state.counter}</h1>
+
+          {/* <button
+            onClick={() => this.setState({ counter: this.state.counter + 1 })}
+            className={"buttonAdd"}
+          >
+            +
+          </button> */}
+
+          <button
+            onClick={() => this.setState({ counter: this.state.counter - 1 })}
+            className="buttonMinus"
+          >
+            -
+          </button>
+
+          <button
+            onClick={() => this.setState({ counter: this.state.counter - 10 })}
+            className="buttonMinus"
+          >
+            -10
+          </button>            
+
+          <button className="buttonReset" onClick={() => this.setState({ counter: 100 })}>
+            Reset
+          </button>
+
+        <div className="divDamage" onClick={ () => this.setState({ counter: this.state.counter - 100})}></div>
+        </div>
+      </div>
+    );
+  }
+
+  addtoCounter = () => {
+    this.setState({
+      counter: this.state.counter + 1,
+    });
+    this.isDead();
+  };
+
+  subtractFromCounter = () => {
+    this.setState({
+      counter: this.state.counter - 1,
+    });
+    this.isDead();
+  };
+
+  isDead = () => {
+    if (this.state.counter < 0) {
+      return true;
+    } else {
+      return false;
+    }
+  }    
+}
